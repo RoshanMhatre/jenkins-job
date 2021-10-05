e82e2e5477a060b51ec46ef2e09cf86a3992055c
mr_rdm <roshanmhatre989@gmail.com>
2021-10-05 13:56:36 +0530

    feat: Add job5 and job6

---
+++ job5.java
@@ -0,0 +1,6 @@
+class job5 {
+	public static void main(String args[]){
+		System.out.println("Hello, I am from job5");
+	}
+	
+}
---
+++ job6.java
@@ -0,0 +1,6 @@
+class job6 {
+	public static void main(String args[]){
+		System.out.println("Hello, I am from job6");
+	}
+	
+}
